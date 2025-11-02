8cbffed3bf956d995603a5de9ed586faee39005b
John Lloyd B. Jardines <jardinesjohnlloyd@gmail.com>
2025-11-02 18:52:11 +0800

    CRITICAL FIX: Add missing settings import in models.py for AUTH_USER_MODEL

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
    
 class Contact(models.Model):
     name = models.CharField(max_length=100)
@@ -8,8 +9,6 @@ class Contact(models.Model):
 
     def __str__(self):
         return self.name
-
-from django.db import models
 
 class UserProfile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
